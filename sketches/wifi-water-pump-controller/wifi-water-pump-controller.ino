1fb6e5af99dac71a036b0a43cfce1782049356ec
rajdeeprath <rajdeeprath@gmail.com>
2018-01-03 01:34:11 +0530

    Pump Controller upgrade - Part 1
    + Better reset mechanism
    + Better initialization
    + Better security
    + Saving all config items to eeprom
    + Using composite relay system to guard againt 'runaway system' upon
    relay failure
    + Explicit monitoring of relay pins using other relay pins
    + Feedback mechanism for relay activation => device running
    + More http apis to manage configurations

--- sketches/wifi-water-pump-controller/wifi-water-pump-controller.ino
+++ sketches/wifi-water-pump-controller/wifi-water-pump-controller.ino
@@ -1,43 +1,52 @@
- #include <ESP8266WiFi.h>          //https://github.com/esp8266/Arduino
+#include <ESP8266WiFi.h>          //https://github.com/esp8266/Arduino
 #include <DNSServer.h>
 #include <ESP8266WebServer.h>
 #include "WiFiManager.h"         //https://github.com/tzapu/WiFiManager
 #include <EEPROM.h>
 
+#define RELAY1 4 
+#define RELAY2 5 
+#define RELAY1_READER 12
+#define RELAY2_READER 14
+#define LIQUID_LEVEL_SENSOR A0 
+#define LED 16 
+#define PUMP_SENSOR 15
 
 const String NAME="HMU-PC-001";
 
-const int RELAY1=4;
-const int LIQUID_LEVEL_SENSOR=A0;
-const int BEEPER=13;
-const int LED=16;
 int DEFAULT_RUNTIME = 10;
 long max_runtime;
 long system_start_time;
 long wait_time = 5000;
 boolean resetFlag=false;
 boolean debug=true;
+boolean inited = false;
 boolean timeover;
 int eeAddress = 0;
 int liquidLevelSensorReadIn = 0;
 int liquidLevelSensorReadInThreshold = 950;
 String switch1state;
-
 boolean LIQUID_LEVEL_OK = true;
+boolean RELAY_ON;
 
-String capailities = "{\"name\":\"HMU-PC-001\",\"description\":\"WATER PUMP CONTROLLER WITH WATER LEVEL SAFETY\",\"devices\":{\"SWITCH1\":{\"get\":\"\/switch\/1\",\"set\":\"\/switch\/1\/set\",\"runtime\":{\"get\":\"\/switch\/1\/runtime\",\"set\":\"\/switch\/1\/runtime\/set?time={value}\"},\"type\":\"switch\",\"states\":[\"on\",\"off\"]},\"sensor1\":{\"index\":1,\"name\":\"LIQUID_LEVEL_SENSOR\",\"get\":\"\/sensor\/1\/get\",\"type\":\"magnetic_float_switch\",\"states\":[\"true\",\"false\"]}},\"global\":{\"actions\":{\"get\":\"\/switch\/all\",\"reset\":\"\/reset\",\"info\":\"\/\"}}}";
+String capailities = "{\"name\":\"" + NAME + "\",\"devices\":{\"name\":\"Irrigation Pump Controller\",\"actions\":{\"getSwitch\":{\"method\":\"get\",\"path\":\"\/switch\/1\"},\"toggleSwitch\":{\"method\":\"get\",\"path\":\"\/switch\/1\/set\"},\"setSwitchOn\":{\"method\":\"get\",\"path\":\"\/switch\/1\/set\/on\"},\"setSwitchOff\":{\"method\":\"get\",\"path\":\"\/switch\/1\/set\/off\"},\"getAllSwitches\":{\"method\":\"get\",\"path\":\"\/switch\/all\"},\"getRuntime\":{\"method\":\"get\",\"path\":\"\/switch\/1\/runtime\"},\"setRuntime\":{\"method\":\"get\",\"path\":\"\/switch\/1\/runtime\",\"params\":[{\"name\":\"time\",\"type\":\"Number\",\"values\":\"60, 80, 100 etc\"}]}}},\"global\":{\"actions\":{\"getNotify\":{\"method\":\"get\",\"path\":\"\/notify\"},\"setNotify\":{\"method\":\"get\",\"path\":\"\/notify\/set\",\"params\":[{\"name\":\"notify\",\"type\":\"Number\",\"values\":\"1 or 0\"}]},\"getNotifyUrl\":{\"method\":\"get\",\"path\":\"\/notify\/url\"},\"setNotifyUrl\":{\"method\":\"get\",\"path\":\"\/notify\/url\/set\",\"params\":[{\"name\":\"url\",\"type\":\"String\",\"values\":\"http:\/\/google.com\"}]},\"reset\":\"\/reset\",\"info\":\"\/\"}}}";
 
 struct Settings {
-   int relay_1;
+   int relay;
    int relay_runtime;
    int led;
-   int lastupdate;
+   long lastupdate;
    long relay_start;
    long relay_stop;
+   int reset = 0;
+   int notify = 1;
+   int endpoint_length;
+   char endpoint[80] = "";
 };
 
 Settings conf = {};
 
+WiFiManager wifiManager;
 std::unique_ptr<ESP8266WebServer> server;
 
 void handleRoot() {
@@ -47,7 +56,8 @@ void handleRoot() {
 
 void handleReset() 
 {
-  resetFlag=true;
+  conf.reset = 1;
+  writeSettings();
   server->send(200, "text/plain", "Resetting in 5 seconds");
 }
 
@@ -71,7 +81,7 @@ void handleNotFound() {
 
 void readAllSwitches()
 {
-  if(conf.relay_1 == 0)
+  if(conf.relay == 0)
   {
     switch1state="STATE=OFF";
   }
@@ -134,9 +144,9 @@ void setSwitchRuntime()
 }
 
 
-void readSwitchA()
+void readSwitch()
 {
-  if(conf.relay_1 == 0)
+  if(conf.relay == 0)
   {
     switch1state="STATE=OFF";
   }
@@ -149,18 +159,20 @@ void readSwitchA()
 }
 
 
-void toggleSwitchA()
+void toggleSwitch()
 {
   checkAndRespondToRelayConditionSafeGuard();
 
-  if(conf.relay_1 == 0)
+  if(conf.relay == 0)
   {
-    relayOn();
+    //relayOn();
+    switchOnCompositeRelay();
     switch1state="STATE=ON";
   }
   else
   {
-    relayOff();
+    //relayOff();
+    switchOffCompositeRelay();
     switch1state="STATE=OFF";
   }
 
@@ -174,7 +186,8 @@ void switchAOn()
 {
   checkAndRespondToRelayConditionSafeGuard();
   
-  relayOn();
+  //relayOn();
+  switchOnCompositeRelay();
     
   server->send(200, "text/plain", "STATE=ON");
 }
@@ -186,27 +199,103 @@ void switchAOff()
 {
   checkAndRespondToRelayConditionSafeGuard();
   
-  relayOff();
+  //relayOff();
+  switchOffCompositeRelay();
 
   server->send(200, "text/plain", "STATE=OFF");
 }
 
 
-
-void relayOn()
+void getNotify()
 {
-  conf.relay_1=1;
-  conf.relay_start = millis();
-  digitalWrite(RELAY1, LOW);
+  readSettings();
+
+  int notify = conf.notify;
+  server->send(200, "text/plain", "NOTIFY=" + String(notify));
 }
 
 
-void relayOff()
+
+
+void setNotify()
 {
-  conf.relay_1=0;
-  conf.relay_stop = millis();
-  digitalWrite(RELAY1, HIGH);
+  int notify;
+
+  if (server->hasArg("notify"))
+  {
+    notify = String(server->arg("notify")).toInt();
+
+    if (notify == 0 || notify == 1)
+    {
+      conf.notify = notify;
+      writeSettings();
+      server->send(200, "text/plain", "notify=" + String(conf.notify));      
+    }
+    else
+    {
+      server->send(400, "text/plain", "Invalid notify value");
+    }
+  }
+  else
+  {
+    server->send(400, "text/plain", "No value provided");
+  }
 }
+
+
+void getNotifyURL()
+{
+  String url;
+
+  readSettings();
+
+  url = conf.endpoint;
+
+  if (url.length() < 4)
+  {
+    server->send(400, "text/plain", "Invalid url value");
+  }
+  else
+  {
+    server->send(200, "text/plain", "URL=" + url);
+  }
+}
+
+
+
+void setNotifyURL()
+{
+  String url;
+
+  if (server->hasArg("url"))
+  {
+    url = String(server->arg("url"));
+
+    if (url.length() < 4)
+    {
+      server->send(400, "text/plain", "Invalid url value");
+    }
+    else
+    {
+      char tmp[url.length() + 1];
+      url.toCharArray(tmp, url.length() + 1);
+
+      conf.endpoint_length = url.length();
+
+      memset(conf.endpoint, 0, sizeof(conf.endpoint));
+      strncpy(conf.endpoint, tmp, strlen(tmp));
+
+      writeSettings();
+      
+      server->send(200, "text/plain", "url=" + url);
+    }
+  }
+  else
+  {
+    server->send(400, "text/plain", "No value provided");
+  }
+}
+
 
 
 void ledOn()
@@ -238,7 +327,7 @@ void checkAndRespondToRelayConditionSafeGuard()
 
 void relayConditionSafeGuard()
 {
-  liquidLevelSensorReadIn = analogRead(LIQUID_LEVEL_SENSOR);
+  liquidLevelSensorReadIn = analogRead(LIQUID_LEVEL_SENSOR);  
   //Serial.println(String(liquidLevelSensorReadIn) + "LIQUID_LEVEL_OK = " + String(LIQUID_LEVEL_OK));
   if(liquidLevelSensorReadIn >= liquidLevelSensorReadInThreshold) // open magnetic switch
   {
@@ -260,47 +349,102 @@ void relayConditionSafeGuard()
 
   /****************************************************************/
 
-  if(conf.relay_1 == 1)
+  if(conf.relay == 1)
   {
     max_runtime = conf.relay_runtime * 1000;
     timeover = ((millis() - conf.relay_start) > max_runtime);
     
     if(!LIQUID_LEVEL_OK || timeover)
     {
-      relayOff();
+      //relayOff();
+      switchOffCompositeRelay();
     }
   }
 }
 
 
+void switchOffCompositeRelay()
+{
+  if(RELAY_ON){    
+    conf.relay=0;
+    conf.relay_stop = millis();
+    digitalWrite(RELAY1, LOW);
+    digitalWrite(RELAY2, HIGH);
+    RELAY_ON = false;
+  }
+}
+
+
+void switchOnCompositeRelay()
+{
+  if(!RELAY_ON){
+    conf.relay=1;
+    conf.relay_start = millis();    
+    digitalWrite(RELAY1, HIGH);
+    digitalWrite(RELAY2, LOW);
+    RELAY_ON = true;
+  }
+}
+
+
+boolean isCompositeRelayOn()
+{
+  int relay_1_state = digitalRead(RELAY1_READER);
+  int relay_2_state = digitalRead(RELAY2_READER);
+
+  if(relay_2_state == 1 && relay_1_state == 0){
+    return true;
+  }
+  else{
+    return false;
+  }
+}
+
+
+boolean isPumpRunning()
+{
+  int sensor_state = digitalRead(PUMP_SENSOR);
+  if(sensor_state == HIGH){
+    return true;
+  }else{
+    return false;
+  }
+}
+
 
 void setup() {
 
-  system_start_time = millis();
+  Serial.begin(9600);
 
-  conf.relay_1=0;
-  conf.led=1;
+  // start eeprom
+  EEPROM.begin(512);
 
-  pinMode(RELAY1, OUTPUT);
-  digitalWrite(RELAY1, HIGH);
+  // Check for reset and do reset routine
+  readSettings();
+  if(conf.reset == 1){
+    debugPrint("Reset flag detected!");    
+    doReset();
+  }
 
+  pinMode(RELAY1, OUTPUT); 
+  digitalWrite(RELAY1, LOW);
+  
+  pinMode(RELAY2, OUTPUT);
+  digitalWrite(RELAY2, HIGH);
+
+  pinMode(RELAY1_READER, INPUT);
+  pinMode(RELAY2_READER, INPUT);
   pinMode(LIQUID_LEVEL_SENSOR, INPUT);
 
   pinMode(LED, OUTPUT);
   digitalWrite(LED, LOW);  
 
-  // put your setup code here, to run once:
-  Serial.begin(115200);
+  pinMode(PUMP_SENSOR, INPUT);
 
-  // start state
-  LIQUID_LEVEL_OK = true;
 
-  // start eeprom
-  EEPROM.begin(512);
-  initSettings();
-  
-  WiFiManager wifiManager;
-  wifiManager.autoConnect();
+  char APNAME[NAME.length() + 1];
+  NAME.toCharArray(APNAME, NAME.length() + 1);
+  wifiManager.autoConnect(APNAME, "iot@123!");
 
   //if you get here you have connected to the WiFi
   debugPrint("connected...yeey :)");
@@ -310,14 +454,17 @@ void setup() {
   server->on("/", handleRoot);
   server->on("/reset", handleReset);
 
-
-  server->on("/switch/1", readSwitchA);
-  server->on("/switch/1/set", toggleSwitchA);
+  server->on("/switch/1", readSwitch);
+  server->on("/switch/1/set", toggleSwitch);
   server->on("/switch/1/set/on", switchAOn);
   server->on("/switch/1/set/off", switchAOff);
   server->on("/switch/1/runtime", getSwitchRuntime);
   server->on("/switch/1/runtime/set", setSwitchRuntime);
   server->on("/switch/all", readAllSwitches);
+  server->on("/notify", getNotify);
+  server->on("/notify/set", setNotify);
+  server->on("/notify/url", getNotifyURL);
+  server->on("/notify/url/set", setNotifyURL);
   
   server->onNotFound(handleNotFound);
   server->begin();
@@ -328,17 +475,17 @@ void setup() {
 
 void loop() 
 {
-    if(resetFlag)
-    {
-      resetFlag = false;
-      
-      delay(5000);
-      
-      WiFiManager wifiManager;
-      wifiManager.resetSettings();
+    if(!inited){
+        inited = true;
+        initSettings();
+    }
 
-      eraseSettings(); 
-      ESP.reset();
+    
+
+    if (conf.reset == 1)
+    {
+      delay(5000);
+      ESP.restart();
     }
     else
     {
@@ -354,6 +501,25 @@ void loop()
 
 
 
+void doReset()
+{
+  conf.relay = 0;
+  conf.relay_runtime = 60;
+  conf.led = 1;
+  conf.lastupdate = 0;
+  conf.relay_start = 0;
+  conf.relay_stop = 0;
+  conf.endpoint_length = 0;
+  conf.reset = 0;
+  conf.notify = 0;
+  memset(conf.endpoint, 0, sizeof(conf.endpoint));
+  
+  eraseSettings();   
+  delay(1000);
+  wifiManager.resetSettings();
+  delay(1000);
+}
+
 
 
 void initSettings()
@@ -362,9 +528,31 @@ void initSettings()
 
   if(conf.relay_runtime <= 0)
   {
+    debugPrint("Setting defaults");
+    
+    String url = "0.0.0.0";
+    char tmp[url.length() + 1];
+    url.toCharArray(tmp, url.length() + 1);
+
+    conf.endpoint_length = url.length();
+    memset(conf.endpoint, 0, sizeof(conf.endpoint));
+    strncpy(conf.endpoint, tmp, strlen(tmp));
+    
+    conf.notify = 0;
     conf.relay_runtime = DEFAULT_RUNTIME;
-    writeSettings();
-  }
+  }  
+
+  system_start_time = millis();
+  conf.relay=0;
+  conf.led=1;
+  conf.lastupdate = 0;
+  conf.relay_start = 0;
+  conf.relay_stop = 0;
+
+  // start state
+  LIQUID_LEVEL_OK = true;
+
+  writeSettings();
 }
 
 
@@ -372,12 +560,11 @@ void initSettings()
 
 void eraseSettings()
 {
-  EEPROM.begin(512);
-
-  for (int i = 0; i < 512; i++)
-  EEPROM.write(i, 0);
-
-  EEPROM.end();
+  debugPrint("Erasing eeprom...");
+  
+  for (int i = 0; i < 512; i++){
+    EEPROM.write(i, 0);
+  }
 }
 
 
@@ -386,33 +573,98 @@ void writeSettings()
 {
   eeAddress = 0;
   conf.lastupdate = millis();
-  
+
+  EEPROM.write(eeAddress, conf.relay);
+  eeAddress++;  
   EEPROM.write(eeAddress, conf.relay_runtime);
   eeAddress++;
+  EEPROM.write(eeAddress, conf.led);
+  eeAddress++;
   EEPROM.write(eeAddress, conf.lastupdate);
+  eeAddress++;
+  EEPROM.write(eeAddress, conf.relay_start);
+  eeAddress++;
+  EEPROM.write(eeAddress, conf.relay_stop);
+  eeAddress++;
+  EEPROM.write(eeAddress, conf.reset);
+  eeAddress++;
+  EEPROM.write(eeAddress, conf.notify);
+  eeAddress++;
+  EEPROM.write(eeAddress, conf.endpoint_length);
+
+  eeAddress++;
+  writeEEPROM(eeAddress, conf.endpoint_length, conf.endpoint);
+
   EEPROM.commit();
   
   debugPrint("Conf saved");
+  debugPrint(String(conf.relay));
   debugPrint(String(conf.relay_runtime));
+  debugPrint(String(conf.led));
   debugPrint(String(conf.lastupdate));
+  debugPrint(String(conf.relay_start));
+  debugPrint(String(conf.relay_stop));
+  debugPrint(String(conf.reset));
+  debugPrint(String(conf.notify));
+  debugPrint(String(conf.endpoint_length));
+  debugPrint(String(conf.endpoint));;
 }
 
 
+void writeEEPROM(int startAdr, int len, char* writeString) {
+  //yield();
+  for (int i = 0; i < len; i++) {
+    EEPROM.write(startAdr + i, writeString[i]);
+  }
+}
 
 
 void readSettings() 
 {
   eeAddress = 0;
   
+  conf.relay = EEPROM.read(eeAddress);
+  eeAddress++;
   conf.relay_runtime = EEPROM.read(eeAddress);
   eeAddress++;
+  conf.led = EEPROM.read(eeAddress);
+  eeAddress++;
   conf.lastupdate = EEPROM.read(eeAddress);
+  eeAddress++;
+  conf.relay_start = EEPROM.read(eeAddress);
+  eeAddress++;
+  conf.relay_stop = EEPROM.read(eeAddress);
+  eeAddress++;
+  conf.reset = EEPROM.read(eeAddress);
+  eeAddress++;
+  conf.notify = EEPROM.read(eeAddress);
+  eeAddress++;
+  conf.endpoint_length = EEPROM.read(eeAddress);
+
+  eeAddress++;
+  readEEPROM(eeAddress, conf.endpoint_length, conf.endpoint);
 
   debugPrint("Conf read");
+  debugPrint(String(conf.relay));
   debugPrint(String(conf.relay_runtime));
+  debugPrint(String(conf.led));
   debugPrint(String(conf.lastupdate));
+  debugPrint(String(conf.relay_start));
+  debugPrint(String(conf.relay_stop));
+  debugPrint(String(conf.reset));
+  debugPrint(String(conf.notify));
+  debugPrint(String(conf.endpoint_length));
+  debugPrint(String(conf.endpoint));
 }
 
+
+
+void readEEPROM(int startAdr, int maxLength, char* dest) {
+
+  for (int i = 0; i < maxLength; i++) {
+    dest[i] = char(EEPROM.read(startAdr + i));
+  }
+}
 
 
 void debugPrint(String message){
