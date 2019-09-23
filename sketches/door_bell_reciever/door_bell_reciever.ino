95a254241468b6ef0dcb6fdbe4a77dfdf3ecd0ba
Connessione Tech <connessionetechnologies@gmail.com>
2019-09-23 16:38:17 +0530

    RF Door Bell Receiver
    + First commit

---
+++ sketches/door_bell_reciever/door_bell_reciever.ino
@@ -0,0 +1,101 @@
+#include <RCSwitch.h>
+#include <ArduinoLog.h>
+
+#define BELL_RELAY_PIN 6
+
+const unsigned int RFCODE = 73964;
+const unsigned long BELL_PRESS_THRESHOLD = 2000;
+
+boolean BELL_RELAY_ON = false;
+unsigned long last_bell_on = 0;
+
+
+RCSwitch mySwitch = RCSwitch();
+
+
+void setup() {
+  Serial.begin(9600);
+
+  Log.begin(LOG_LEVEL_NOTICE, &Serial);
+  
+  Log.notice("Initializing...." CR);
+  mySwitch.enableReceive(0);  // Receiver on interrupt 0 => that is pin #2
+}
+
+
+void loop() 
+{
+  if (mySwitch.available()) 
+  {  
+    int value = mySwitch.getReceivedValue();
+    
+    if (value == 0) 
+    {
+      Serial.print("Unknown encoding");
+    } 
+    else 
+    {
+      Serial.print("Received ");
+      Serial.print( mySwitch.getReceivedValue() );
+      Serial.print(" / ");
+      Serial.print( mySwitch.getReceivedBitlength() );
+      Serial.print("bit ");
+      Serial.print("Protocol: ");
+      Serial.println( mySwitch.getReceivedProtocol() );
+    }
+
+    mySwitch.resetAvailable();
+  }
+}
+
+
+/**
+ * Is Bell press time over ?. This is needed to simulate bell press.
+ * Might vary from bell to bell. Adjust according to need.
+ */
+void is_bell_press_time_over()
+{
+  unsigned int curr = millis();
+
+  if(curr > 0)
+  {
+      if(curr - last_bell_on > BELL_PRESS_THRESHOLD)
+      {
+        return true;
+      }
+  }
+  else if(curr == 0)
+  {
+      last_bell_on = 0;
+  }  
+}
+
+
+
+/**
+ * Switch PIN to HIGH to TRIGGER RELAY that will send current to bell
+ */
+void bell_on()
+{
+  if(BELL_RELAY_ON == false)
+  {
+      Log.notice("Switching on bell...." CR);
+      digitalWrite(BELL_RELAY_PIN, HIGH);
+      BELL_RELAY_ON = true;
+  }
+}
+
+
+
+/**
+ * Switch PIN to LOW to TRIGGER RELAY that will send current to bell
+ */
+void bell_off()
+{
+  if(BELL_RELAY_ON == true)
+  {
+      Log.notice("Switching off bell...." CR);
+      digitalWrite(BELL_RELAY_PIN, LOW);
+      BELL_RELAY_ON = false;
+  }
+}
